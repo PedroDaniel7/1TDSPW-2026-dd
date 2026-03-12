1d794ea3b6f3b2908fee6da421faa11342b971da
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-11 22:45:48 -0300

    Projeto da apostila 03

---
+++ src/Carro.java
@@ -0,0 +1,11 @@
+public class Carro {
+
+    String modelo;
+    String Cor = "green";
+    int VelocidadeMaxima = 190;
+    double Peso = 500.398;
+    boolean Automatico;
+    short Ano = 2008;
+    float Altura;
+
+}
---
+++ src/Teste.java
@@ -0,0 +1,21 @@
+public class Teste {
+
+    public static void main(String[] args){
+        System.out.println("Boa noite!");
+
+        //Declarar variáveis
+        boolean ativo;
+        //Atribuir um valor a variável ativo
+        ativo = true; //false
+        //Declarar variáveis e atribuir o valor
+        char sexo = 'F';
+        byte idade;
+        short rm = 3333;
+        int ano = 2026;
+        long id = 123;
+        float altura = 1.80f;
+        double preco = 150.25;
+        String nome = "Ricardo";
+
+    }
+}
